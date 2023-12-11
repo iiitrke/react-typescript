4330661a90881d205b40b131e11622d9bc3f47c8
Itasca Technologies <info@itasca.co.in>
2023-12-11 13:46:39 +0530

    MobileNav is working

--- src/components/navbar/MobileNav.tsx
+++ src/components/navbar/MobileNav.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { menuItemsData } from "../../menuItemsData";
+import { MobileMenuItems } from "./MobileMenuItems";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  const depthLevel = 0;
+  const [showMenu, setShowMenu] = useState(false);
+  let ref: any = useRef();
+
+  useEffect(() => {
+    const handler = (event: any) => {
+      if (showMenu && ref.current && !ref.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    document.addEventListener("touchstart", handler);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", handler);
+      document.removeEventListener("touchstart", handler);
+    };
+  }, [showMenu]);
+
+  return (
+    <nav className="mobile-nav">
+      <button
+        className="mobile-nav__menu-button"
+        type="button"
+        onClick={() => setShowMenu((prev) => !prev)}
+      >
+        Menu
+      </button>
+
+      {showMenu && (
+        <ul className="menus" ref={ref}>
+          {menuItemsData.map((menu, index) => {
+            return (
+              <MobileMenuItems
+                items={menu}
+                key={index}
+                depthLevel={depthLevel}
+                showMenu={showMenu}
+                setShowMenu={setShowMenu}
+              />
+            );
+          })}
+        </ul>
+      )}
+    </nav>
+  );
 };
 
 export { MobileNav };
