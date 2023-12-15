3ae4448c39752009703c276f18ab36b3ca4f1219
Itasca Technologies <info@itasca.co.in>
2023-12-15 20:58:51 +0530

    Intro Page Working

--- src/components/intro/IntroComp.tsx
+++ src/components/intro/IntroComp.tsx
@@ -4,9 +4,10 @@ import { useIntroAction } from "../../state/hooks/useIntroAction";
 import { useTypedSeletor } from "../../state/hooks/useTypedSelector";
 import { RootState } from "../../state";
 import { IntroState } from "../../state/reducers/introReducer";
-
+import { IntroModel } from "../../state/models/Intro-type";
 import ReactMarkdown from "react-markdown";
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import "./Intro.css";
 const IntroComp = () => {
   const { searchRepositories } = useUserActions();
   const { introRepository } = useIntroAction();
@@ -19,20 +20,28 @@ const IntroComp = () => {
     console.log("HandleFetchUser in UserPage");
   }
   return (
-    <div>
-      {data.map((intro, index) => (
-        <div>
-          <Card key={index}>
-            <CardContent>
-              <ReactMarkdown children={intro.title} />
-            </CardContent>
-          </Card>
-        </div>
-      ))}
+    <>
+      <div className="intro">
+        {data.map((intro: IntroModel, index) => (
+          <div>
+            <Card key={index}>
+              <CardMedia
+                component="img"
+                image={`./images/intro/${intro.image}`}
+              />
+
+              <CardContent>
+                <ReactMarkdown children={intro.title} />
+                <ReactMarkdown children={intro.desc} />
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </div>
       <button className="btn" onClick={handleFetchUser}>
         Fetch User1
       </button>
-    </div>
+    </>
   );
 };
 
