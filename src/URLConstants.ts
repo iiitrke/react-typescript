fbb49a7584a176c7dc79ebcb46eebd330e4ef2c8
Itasca Technologies <info@itasca.co.in>
2023-12-19 18:02:00 +0530

    intomd

--- src/URLConstants.ts
+++ src/URLConstants.ts
@@ -1,6 +1,6 @@
 export const HOSTNAME = "http://localhost:8000";
 export const API_TRG = "/api/ui/trg"; //`${HOSTNAME}/trg`;
 export const API_USERS = "/api/ui/users"; //`${HOSTNAME}/users`;
-export const API_INTRO = "/api/ui/intro-md"; //`${HOSTNAME}/intro-md`;
+export const API_INTRO = "/api/ui/intromd"; //`${HOSTNAME}/intromd`;
 
 export const API_FEATURED_COURSES = "/api/featuredcourse"; //`${HOSTNAME}/featuredcourse`;
