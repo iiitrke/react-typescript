54d78021cbe09b3ec399ebde60392ecd3f742b8f
Itasca Technologies <info@itasca.co.in>
2023-12-19 16:44:40 +0530

    changes

--- src/URLConstants.ts
+++ src/URLConstants.ts
@@ -1,6 +1,6 @@
 export const HOSTNAME = "http://localhost:8000";
-export const API_TRG = `${HOSTNAME}/trg`;
-export const API_USERS = `${HOSTNAME}/users`;
-export const API_INTRO = `${HOSTNAME}/intro-md`;
+export const API_TRG = "api/trg"; //`${HOSTNAME}/trg`;
+export const API_USERS = "/api/users"; //`${HOSTNAME}/users`;
+export const API_INTRO = "/api/intro-md"; //`${HOSTNAME}/intro-md`;
 
-export const API_FEATURED_COURSES = `${HOSTNAME}/featuredcourse`;
+export const API_FEATURED_COURSES = "/api/featuredcourse"; //`${HOSTNAME}/featuredcourse`;
