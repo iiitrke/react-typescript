a89266f9dff7622f9f2844d919982a3fe143b0c1
Itasca Technologies <info@itasca.co.in>
2023-12-19 16:23:22 +0530

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import {useRouter} from 'next/router';
 import { AppRootLayout } from './layout';
+import { BannerFC } from '../components/banner/BannerFC';
 export default function Page() {
 
 const navigation = useRouter();
@@ -7,6 +8,7 @@ const navigation = useRouter();
     return (
 <AppRootLayout>
      <p>Hello world</p>
+     <BannerFC />
      </AppRootLayout> 
     )
   }
