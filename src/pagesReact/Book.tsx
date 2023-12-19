36db8984c7c5c6ee3d4fe9da1e210d70b74ee6c0
Itasca Technologies <info@itasca.co.in>
2023-12-19 16:17:39 +0530

    removed react-router

--- src/pagesReact/Book.tsx
+++ src/pagesReact/Book.tsx
@@ -1,14 +1,19 @@
-import React from "react"
-import { useParams } from "react-router"
+import React from "react";
+// import { useParams } from "react-router"
 
-export const BookAsFC: React.FC = ()=>{
-    const {id} =useParams();
+export const BookAsFC: React.FC = () => {
+  // const {id} =useParams();
 
-    return (
-        <>
-        Book Page {id}
-        </>
-    )
+  return (
+    <>
+      Book Page
+      {/* {id} */}
+    </>
+  );
 
-    return <div><p> Books</p></div>
-}
+  return (
+    <div>
+      <p> Books</p>
+    </div>
+  );
+};
