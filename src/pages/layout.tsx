9ee259846a3d0648d6f92eff797673376dff4e36
Itasca Technologies <info@itasca.co.in>
2023-12-19 17:58:53 +0530

    provider

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import { AppBarFC } from "../components/navbar/AppBarFC";
 import { FooterAsFC } from "../components/Footer";
+import { Provider } from "react-redux";
+import { store } from "../state";
 
 const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
-        <AppBarFC />
-        {children}
+        <Provider store={store}>
+          <AppBarFC />
+          {children}
+        </Provider>
       </body>
     </html>
   );
