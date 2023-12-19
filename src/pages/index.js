fbd942c4d8f99048686b6e4d0ecd580f09a0fe34
Itasca Technologies <info@itasca.co.in>
2023-12-19 17:46:23 +0530

    API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import {useRouter} from 'next/router';
  import AppRootLayout from './layout';
-import { BannerFC } from '../components/banner/BannerFC';
+import { BannerFC } from '../components/banner/BannerFC'; 
+import { IntroFC } from '../components/intro/IntroFC';
 export default function Page() {
 
 const navigation = useRouter();
 
     return (
 <AppRootLayout>
-     <p>Hello world</p>
      <BannerFC />
+     <IntroFC />
      </AppRootLayout> 
     )
   }
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import { AppBarFC } from "../components/navbar/AppBarFC";
+import { FooterAsFC } from "../components/Footer";
 
 const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -8,6 +9,7 @@ const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
       <body>
         <AppBarFC />
         {children}
+        <FooterAsFC />
       </body>
     </html>
   );
