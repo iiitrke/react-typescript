413fcdf04c301a22947d34687fb56d5e3838af81
Itasca Technologies <info@itasca.co.in>
2023-12-19 17:30:15 +0530

    Ui API

--- src/pages/api/ui/intro.ts
+++ src/pages/api/ui/intro.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
-import { introData } from "../../data/introData";
+import { introData } from "../../../data/introData";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
   switch (method) {
