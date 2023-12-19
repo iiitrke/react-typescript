c0f8e775c7a3540170776f522d3180579b516cfb
Itasca Technologies <info@itasca.co.in>
2023-12-19 16:27:21 +0530

    test3

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,3 +12,5 @@ const navigation = useRouter();
      </AppRootLayout> 
     )
   }
+
+  
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 
-export const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
+const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
@@ -11,3 +11,5 @@ export const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
     </html>
   );
 };
+
+export default AppRootLayout;
