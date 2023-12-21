4cb32075196b08acae978d94170688e107dae293
Itasca Technologies <info@itasca.co.in>
2023-12-21 11:46:33 +0530

    migrate

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import {useRouter} from 'next/router';
 import { BannerFC } from '../components/banner/BannerFC'; 
 import { IntroFC } from '../components/intro/IntroFC';
 import { LoginFormCL, LoginFormFC } from '../components/login/LoginFormCL';
+ 
 export default function Page() {
 
+ 
 const navigation = useRouter();
+
+
 
     return (
 <AppRootLayout>
