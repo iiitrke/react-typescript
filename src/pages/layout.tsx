df9bcbc7bbcaa4a6ef0c1266f4dbcb73d0b3033b
Itasca Technologies <info@itasca.co.in>
2023-12-22 01:57:17 +0530

    dfdf

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -5,7 +5,6 @@ import { FooterAsFC } from "../components/Footer";
 import { Provider } from "react-redux";
 import { store } from "../state";
 
-<<<<<<< HEAD
 const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
@@ -13,10 +12,6 @@ const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
       {children}
     </Provider>
   );
-=======
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
->>>>>>> 5f988e57903be787bca27559f2e79fbf6830842a
 };
 
-export default RootLayout;
+export default AppRootLayout;
