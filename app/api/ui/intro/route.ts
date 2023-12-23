38f7e89733a5ce701f359ee7ff2151c4ecaa0d5e
Itasca Technologies <info@itasca.co.in>
2023-12-23 14:11:33 +0530

    xzcxzc

--- app/api/ui/intro/route.ts
+++ app/api/ui/intro/route.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
 import { introData } from "../../../../data/introData";
 
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // To handle a GET request to /api
-export function GET(request: NextApiRequest) {
+export function GET(request: NextRequest) {
   // Do whatever you want
   return NextResponse.json(introData, { status: 200 });
 }
