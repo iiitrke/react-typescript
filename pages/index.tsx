cd89447dabf2f365df776ec18d52bff7d30a4637
Itasca Technologies <info@itasca.co.in>
2023-12-23 06:01:32 +0530

    hjh

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import AppRootLayout from "./layout";
+import { BannerFC } from "../src/components/banner/BannerFC";
+import { IntroFC } from "../components/intro/IntroFC";
+import { LoginFormCL, LoginFormFC } from "../components/login/LoginFormCL";
+export default function Page() {
+  const navigation = useRouter();
+  return (
+    <AppRootLayout>
+      <BannerFC />
+      <IntroFC />
+
+      <LoginFormCL />
+    </AppRootLayout>
+  );
+}
---
+++ pages/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import { AppBarFC } from "../components/navbar/AppBarFC";
+import { FooterAsFC } from "../components/Footer";
+import { Provider } from "react-redux";
+import { store } from "../state";
+
+const AppRootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <AppBarFC />
+      {children}
+    </Provider>
+  );
+};
+
+export default AppRootLayout;
