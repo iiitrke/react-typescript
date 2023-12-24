2647c33c14e72a2bf003bfd4f3abdcdc206d4111
Itasca Technologies <info@itasca.co.in>
2023-12-24 12:23:19 +0530

    started seed working

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -2,31 +2,24 @@ import { NextApiRequest, NextApiResponse } from "next/types";
 import { introData } from "../../../src/data/introData";
 import prisma from "../../../src/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-
+import { Intro } from "../../../src/generated/client";
 // To handle a GET request to /api
 export async function GET(request: NextRequest) {
-  //   const datas = prisma.intro.createMany({ data: introData }).then((result) => {
-  //     console.log(result);
-  //     return result;
-  //   });
-  //   const datas = introData.map((intro) => {
-  //     prisma.intro.create({ data: intro }).then((value) => value);
-  //     console.log(intro);
-  //   });
-  //   console.log(datas);
+  const deleteIntros = prisma.intro.deleteMany();
+  await prisma.$transaction([deleteIntros]);
 
-  //   const deletedIntro = await prisma.intro
-  //     .deleteMany({})
-  //     .then((count) => console.log(count));
-
-  const datas = await introData.map((intro) => {
-    return prisma.intro.create({ data: intro }).then((data) => data);
+  const datas: Promise<Intro>[] = introData.map<Promise<Intro>>((intro) => {
+    return prisma.intro.create({ data: intro });
+    // .then((result) => result);
   });
+  const resolvedPromises = await Promise.all(datas);
+  console.log("DATA STORED1");
+  console.log(resolvedPromises);
 
   //   const deleteIntros = prisma.intro.deleteMany();
   //   await prisma.$transaction([deleteIntros]);
   return NextResponse.json(
-    { count: "Deleted", data: "Deleted" },
+    { count: "Deleted", data: resolvedPromises },
     { status: 200 }
   );
 }
